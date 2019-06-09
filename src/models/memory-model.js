566dbe249203deea9f355379758a8418408bfce3
Matt Wilkin <mwilkin22@gmail.com>
2019-06-09 16:21:06 -0700

    add docs for memory-model.

--- src/models/memory-model.js
+++ src/models/memory-model.js
@@ -2,12 +2,32 @@
 
 const uuid = require('uuid/v4');
 
+/**
+* @module src/models/memory-model
+ */
+
+/**
+ * @Class Memory Model
+ * @desc memory model for count, get, post, delete and put requests
+  */
+
 class Model {
+  
+  /**
+  * @method constructor
+  * @param {categories} schema - schema from categories
+   */
 
   constructor(schema) {
     this.schema = schema;
     this.database = [];
   }
+
+  /**
+  * @method sanitize
+  * @param {object} entry - Entry to be sanitized
+  * @desc Sanitize entry
+   */
 
   sanitize(entry) {
 
@@ -30,14 +50,31 @@ class Model {
     return valid ? record : undefined;
   }
   
+  /**
+  * @method count
+  * @desc Keep count of how many entries in the database
+   */
+
   count() {
     return this.database.length;
   }
+
+  /**
+  * @method get
+  * @param {string} id - Id for specific entries
+  * @desc Keep count of how many entries in the database
+   */
 
   get(id) {
     const records = id ? this.database.filter( (record) => record._id === id ) : this.database;
     return Promise.resolve(records);
   }
+
+  /**
+  * @method post
+  * @param {string} entry - information about what should be added
+  * @desc Add a entry to the database
+   */
 
   post(entry) {
     entry._id = uuid();
@@ -46,17 +83,35 @@ class Model {
     return Promise.resolve(record);
   }
 
+  /**
+  * @method delete
+  * @param {string} id - Id for specific entries
+  * @desc Delete a particular entry from the database
+   */
+
   delete(id) {
     this.database = this.database.filter((record) => record._id !== id );
     return this.get(id);
   }
+
+  /**
+  * @method put
+  * @param {string} id - Id for specific entries
+  * @param {string} entry - Information that should be added or changed
+  * @desc Modify one entry in the database
+   */
 
   put(id, entry) {
     let record = this.sanitize(entry);
     if( record._id ) { this.database = this.database.map((item) => (item._id === id) ? record : item  ); }
     return this.get(id);
   }
-  
+
 }
+
+/**
+* Export object
+* @type {Object}
+ */
 
 module.exports = Model;
