6882cd2cfccd50695153ec5b0a3e6aff7ad89170
Matt Wilkin <mwilkin22@gmail.com>
2019-06-09 16:23:54 -0700

    add docs players-model.

--- src/models/players/players-model.js
+++ src/models/players/players-model.js
@@ -3,7 +3,21 @@
 const Model = require('../mongo-model.js');
 const schema = require('./players-schema.js');
 
+/**
+* @module src/models/teams/teams-model
+ */
+
+/**
+* @Class Teams Model
+* @property {class} - class extends from model
+ */
+
 class Players extends Model {}
+
+/**
+* Export object
+* @type {Object}
+ */
 
 module.exports = new Players(schema);
 
